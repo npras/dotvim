27c8dcd4cecd9cbeb57767da2a5d2ffb9bd157e8
Prasanna Natarajan <prasann87@gmail.com>
2014-07-26 16:54:53 +0530

    first commit

---
+++ Rakefile.rb
@@ -0,0 +1,46 @@
+desc "Remove if already exists"
+task :destroy do
+  rm_rf File.join(ENV['HOME'], ".vimrc")
+  rm_rf File.join(ENV['HOME'], ".gvimrc")
+  rm_rf File.join(ENV['HOME'], ".vim")
+end
+
+desc "Symlink .vimrc and .gvimrc files"
+task :rc_files do
+  %w[vimrc gvimrc].each do |script|
+    dotfile = File.join(ENV['HOME'], ".#{script}")
+    if File.exist?(dotfile) || File.symlink?(dotfile)
+      warn "#{dotfile} already exists!"
+    else
+      ln_s File.join("#{ENV['HOME']}/bin/dotfiles/dotvim", script), dotfile
+      puts "vim/#{script} symlinked to #{dotfile}"
+    end
+  end
+end
+
+desc "Symlink .vim directory"
+task :config_dir do
+  config_dir = File.join(ENV['HOME'], ".vim")
+  if Dir.exist? config_dir
+    puts "#{config_dir} already exists!"
+  else
+    ln_s File.join("#{ENV['HOME']}/bin/dotfiles/dotvim", 'vim'), config_dir
+    puts "vim symlinked to #{config_dir}"
+  end
+end
+
+desc "Create ~/.vim-tmp/bckp,swap,undo dirs"
+task :tmp_dirs do
+  mkdir_p File.join(ENV['HOME'], '.vim-tmp', 'bckp')
+  mkdir_p File.join(ENV['HOME'], '.vim-tmp', 'swap')
+  mkdir_p File.join(ENV['HOME'], '.vim-tmp', 'undo')
+end
+
+desc "Source .vimrc and .gvimrc"
+task :source do
+  `. ~/.vimrc`
+  `. ~/.gvimrc`
+end
+
+desc "the setup"
+task :setup => [:rc_files, :config_dir, :tmp_dirs, :source]
---
+++ vim/bundle/scratch.vim/README.rst
@@ -0,0 +1,35 @@
+Scratch.vim
+===========
+
+Unobtrusive scratch window. Inspired by scratch.vim_, enhanced.
+
+.. image:: doc/scratch.png
+   :align: center
+
+
+Features
+--------
+
+* Scratch window automatically hides when inactive
+* Paste selection (character-wise, line-wise or block-wise) directly into 
+  scratch buffer from a single mapping
+* Quick insert mapping
+
+``:help Scratch`` for details and more.
+
+
+Installation
+------------
+
+With `pathogen.vim`_:
+
+.. code:: bash
+
+  $ cd ~/.vim/bundle
+  $ git clone https://github.com/mtth/scratch.vim
+
+Otherwise simply copy the folders into your ``.vim`` directory.
+
+
+.. _pathogen.vim: https://github.com/tpope/vim-pathogen
+.. _scratch.vim: https://github.com/vim-scripts/scratch.vim
